ff5ddad09588e0de2670b67e7ef7a77bf9d337a9
Vui Nguyen <vui_engineer@yahoo.com>
2018-10-02 18:01:05 -0600

    initial UI

---
+++ MemeV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeV1
+//
+//  Created by Vui Nguyen on 10/2/18.
+//  Copyright © 2018 Sunfish Empire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
